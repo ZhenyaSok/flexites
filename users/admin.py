a3d3e7d0e9cfda4b57a0a548bdbc9574129ef436
Jenny <kapitan.kub@gmail.com>
2024-10-16 16:30:30 +0500

    [*] Register app Users in admin

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
-# Register your models here.
+from users.models import User
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    """Модель "Пользователь" для административной панели Django."""
+
+    list_display = ('id', 'first_name', 'last_name', 'email', 'phone', 'role', 'is_active',)
+    search_fields = ('id', 'email')
+    list_filter = ('is_active',)
