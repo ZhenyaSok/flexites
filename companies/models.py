fd62ff79c5883628ecbd8d3a883f6434f00f319d
Jenny <kapitan.kub@gmail.com>
2024-10-16 16:35:02 +0500

    [*] Model companies

--- companies/models.py
+++ companies/models.py
@@ -1,10 +1,9 @@
 from django.db import models
 
-'''Организация:
-- Название
-- Краткое описание'''
+
 NULLABLE = {"null": True, "blank": True}
 
 class Company(models.Model):
+    """ Модель организации"""
     title = models.CharField(max_length=150, verbose_name='Название организации')
     description = models.TextField(verbose_name='Краткое описание', **NULLABLE)
