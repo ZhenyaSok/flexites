69719e4e7a311067fb5a36e1e4ee319dd264976b
Jenny <kapitan.kub@gmail.com>
2024-10-17 16:52:41 +0500

    [~] Upgrade userSerializers for users(list) DONE

--- users/serializers.py
+++ users/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 
+from companies.models import Company
 from users.models import User
 from djoser.serializers import UserCreateSerializer as BaseUserRegistrationSerializer
 from rest_framework import serializers
@@ -15,7 +16,7 @@ class UserRegistrationSerializer(BaseUserRegistrationSerializer):
 
     class Meta:
         model = User
-        fields = ['id', 'first_name', 'phone', 'last_name', 'email', 'password',]
+        fields = ['id', 'first_name', 'phone', 'last_name', 'email', 'password', 'companies',]
 
 
 class CurrentUserSerializer(serializers.ModelSerializer):
@@ -24,4 +25,14 @@ class CurrentUserSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = User
-        fields = ['id', 'first_name', 'last_name', 'email', 'password', 'phone',]
+        fields = ['id', 'first_name', 'last_name', 'email', 'password', 'phone', 'companies',]
+
+
+
+class UserSerializer(serializers.ModelSerializer):
+    """Отображение данных списка пользователей. (Модель User)"""
+    companies = serializers.StringRelatedField(many=True)
+    class Meta:
+
+        model = User
+        fields = ['id', 'first_name', 'last_name', 'email', 'phone', 'companies']
