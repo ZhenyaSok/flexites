1f40d57e3cd8bd0650ac7739220d40c79178b1e9
Jenny <kapitan.kub@gmail.com>
2024-10-17 16:50:16 +0500

    [+] Added __str__ and class Meta

--- companies/models.py
+++ companies/models.py
@@ -7,3 +7,10 @@ class Company(models.Model):
     """ Модель организации"""
     title = models.CharField(max_length=150, verbose_name='Название организации')
     description = models.TextField(verbose_name='Краткое описание', **NULLABLE)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'Организация'
+        verbose_name_plural = 'Организации'
